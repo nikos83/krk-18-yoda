4404c0207778ac59aecddd8a514a120b41ba78da
Nikodem Górak <nikos83@gmail.com>
2018-07-21 15:41:01 +0200

    correct seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 User.destroy_all
 
-User.create!(
+user = User.create!(
   email: 'admin@document.pl',
   password: 'Doc1234'
 )
+
 Bucket.create!(
   name: 'Main',
-  user_id: '1'  
+  user_id: user.id  
 )
 puts "Seeds have been added."
