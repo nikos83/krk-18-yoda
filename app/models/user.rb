4ea439c39deae901752d99953e4636ed5f49f984
Nikodem Górak <nikos83@gmail.com>
2018-07-21 13:44:14 +0200

    Add depedences to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :buckets
+  has_many :documetns, through: :bucket
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
