2050742f40e40610327624a529ac557f352edf83
Nadia <ndebiec@wp.pl>
2018-07-21 14:34:54 +0200

    Added views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
+  root 'welcome#home'
+  get 'digitize', to: 'documents#new'
+  get 'dashboard', to: 'documents#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
