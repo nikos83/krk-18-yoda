a4f7f168986c0c536182019ef1a1ab7b0c6a65a7
Damian Łabas <damian.labas@codibly.com>
2018-07-22 12:53:28 +0200

    sercure upload

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,4 +4,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png pdf)
+  end
+
+
+  def size_range
+    0.1..2.megabytes
+  end
 end
