fcdca5133c54cb2e0ed7511dd0e79f8f4965e57f
katguz3485 <katarzyna.guzik01@gmail.com>
2018-07-22 14:42:52 +0200

    dash view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 
   authenticated :user do
-    root 'welcome#home', as: 'authenticated_root'
+    root 'documents#index', as: 'authenticated_root'
   end
   devise_scope :user do
     root 'devise/sessions#new'
